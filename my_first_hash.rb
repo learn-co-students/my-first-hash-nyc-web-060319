213aefa8ef46ef7183a3ec1e48d7d3ee695aa0ac
github username <github email address>
2019-08-23 19:20:31 -0400

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,29 +1,48 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {"Key1" => 2 , "Key2" => "3"}
 
+  kdc_hash = {
+    "name" => "Kyle"
+  }
 end
 
 
 def shipping_manifest
-  #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information:
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+    }
 end
 
 def retrieval
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+
   shipping_manifest["oil paintings"]
-  #your code here!
 end
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+  # add 2 muskets to the shipping_manifest hash below
+
+  shipping_manifest["muskets"] = 2
+
+
+  # add 4 gun powder to the shipping_manifest hash below
+
+  shipping_manifest["gun powder"] = 4
+
+  # return the shipping_manifest hash below
+
   shipping_manifest
-  #your code here
-  #remember to return the shipping_manifest hash
 
 end
